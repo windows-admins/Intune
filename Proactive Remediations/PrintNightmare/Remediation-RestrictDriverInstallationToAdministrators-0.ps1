825ea3dd318fcedb114781d0286ade6b42e5ecf4
removedporn <86824510+removedporn@users.noreply.github.com>
2022-08-16 09:10:22 +0800

    Delete Proactive Remediations directory

--- Proactive Remediations/PrintNightmare/Remediation-RestrictDriverInstallationToAdministrators-0.ps1
+++
@@ -1,4 +0,0 @@
-if (-not (Test-Path -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Printers\PointAndPrint')) {
-    New-Item -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Printers\PointAndPrint' -Force
-}
-New-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Printers\PointAndPrint' -Name 'RestrictDriverInstallationToAdministrators' -PropertyType DWORD -Value 0 -Force
