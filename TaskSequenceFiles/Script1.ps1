c6b5de65945390fb1f440defd95c7979d2745c77
removedporn <86824510+removedporn@users.noreply.github.com>
2022-08-16 09:10:56 +0800

    Delete TaskSequenceFiles directory

--- TaskSequenceFiles/Script1.ps1
+++
@@ -1,4 +0,0 @@
-$executingScriptDirectory = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
-copy-item $executingScriptDirectory\Scripts $env:ProgramFiles -force -Recurse
-copy-item $executingScriptDirectory\PackageManagement $env:ProgramFiles -force -Recurse
-copy-item $executingScriptDirectory\WindowsPowershell $env:ProgramFiles -force -Recurse
